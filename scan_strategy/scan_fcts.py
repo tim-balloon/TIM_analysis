5ff8f56cd84bfe2d9fc8e536dbc4a7c498073638
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-03-10 12:36:50 -0500

    1st commit

---
+++ scan_strategy/scan_fcts.py
@@ -0,0 +1,330 @@
+import numpy as np
+from TIM_scan_strategy import *
+from IPython import embed
+
+def hitsPerSqdeg(total_hits, area, res):
+    """
+    hits per sqare degree 
+    Parameters
+    ----------
+    total_hits: 2d array
+        hit map   
+    area: float
+        area in deg2
+    res: float
+        resolution in same units as area
+
+    Returns
+    -------
+    hitsPerSqdeg: 2d array
+        map of hits per square degree 
+    """ 
+    return total_hits/(area/res**2)
+
+def timeFractionAbove(hmap, level):
+    """
+    fraction of time above a level of hits
+    Parameters
+    ----------
+    hmap: 2d array
+        hit map   
+    level: float
+        level of hits
+    Returns
+    -------
+    timeFractionAbove: float
+        fraction of the hitmaps above a level of hits
+    """ 
+    hits = hmap.flatten()
+    return np.sum(hits[hits>level])/np.sum(hits)
+
+def genLocalPath(az_size = 1, alt_size = 1, alt_step=0.2, acc = 0.005, scan_v=0.05, dt= 0.01):
+    """
+    Function that generates the local scaning pattern.
+    Currently can only generate closed loop    
+    Parameters
+    ----------
+    az_size: float
+        azimuth angular size, in degrees   
+    alt_size: float
+        altitude angular size, in degrees
+    alt_step: float
+        step in altitude angle, in degrees
+    acc: float
+        acceleration in second angle 
+    scan_v: float
+        angular speed of the scan, in deg/sec
+    dt: float
+        time step in second angle 
+    Returns
+    -------
+    az: array
+        azimuth scan path coordinates, in degrees
+    alt: array
+        altitude scan path coordinates, in degrees
+    flag: array
+        constant scan speed part.
+    scan_eff: array
+        scan efficiency: the ratio between the constant scan speed part and not constant scan speed part
+    t: array
+        time during the scan, in second angle
+    """ 
+    ver_N = int(alt_size//alt_step)
+
+    scan_time = az_size/scan_v
+    turn_time = 2*scan_v/acc
+
+    a = np.concatenate((np.ones(int(turn_time/dt))*acc,np.zeros(int(scan_time/dt))))
+    a = np.concatenate((a,-1*a))
+    a = np.tile(a,ver_N) 
+
+    acc_alt = alt_step/(turn_time/2)**2
+    a2 = np.concatenate((np.ones(int(turn_time/dt/2))*acc_alt,-1*np.ones(int(turn_time/dt/2))*acc_alt,np.zeros(int(scan_time/dt))))
+    a2 = np.concatenate((np.tile(a2,ver_N),np.tile(-1*a2,ver_N)))
+
+    flag = np.where(a==0,1,0) #constant scan speed part
+
+    t = np.arange(0,len(a))*dt
+
+    v = np.cumsum(a)*dt-scan_v
+    az = np.cumsum(v)*dt
+
+    v2 = np.cumsum(a2)*dt
+    alt  = np.cumsum(v2)*dt
+
+    scan_eff = np.sum(flag)/len(az)
+    
+    return az,alt,flag, scan_eff ,t 
+
+def genScanPath(T, alt, az, flag, plot=False):
+    """    
+    Function that generates the pointing coordinates vs time.
+
+    Parameters
+    ----------
+    T: array
+        time stream
+    az: array
+        azimuth scan path coordinates, in degrees
+    alt: array
+        altitude scan path coordinates, in degrees
+    flag: array
+        constant scan speed part. 
+    Returns
+    -------
+    coor: 2d array
+        coordinates in degrees
+    flag: array
+        constant scan speed part. 
+    """ 
+
+    coor = np.zeros((len(T),2))
+
+    idx = np.int_(np.fmod(T,len(alt)/100)*100)
+    
+    coor[:,0] = az[idx]-np.mean(az)
+    coor[:,1] = alt[idx]-np.mean(alt)
+
+    if(plot):
+        plt.figure()
+        plt.plot(az-np.mean(az), alt-np.mean(alt), 'or')
+        plt.plot(coor[:,0],coor[:,1],'ob')
+        plt.show()
+    #flag      = flag[idx]
+
+    return coor,  flag
+
+def pixelOffset(pixel_num, pixel_pitch):
+    """
+    Function that  gernerates the pixel offset vs pointing center
+    Parameters
+    ----------
+    pixel_num: int
+        number of spatial pixels
+    pixel_pitch: float
+        spatial distance between adjacent pixels in degrees
+    Returns
+    -------
+    yoffsets: array
+        the pixel offset vs pointing center, in degrees
+    """ 
+    yoffsets = (np.arange(0,pixel_num)-pixel_num/2)*pixel_pitch
+#     offsets = np.vstack((np.zeros(pixel_num),yoffsets)).T
+    
+    return yoffsets
+
+def genPixelPath(pointing_path, pixel_offset, theta):
+    """
+    Function that gernerates the pointing time stream for each pixel
+    Parameters
+    ----------
+    pointing_path: 2d array
+        coordinates timestream of the pointing
+    pixel_offset: float
+        spatial distance between adjacent pixels in degrees
+    theta: float
+        angle in degree
+    Returns
+    -------
+    pixel_path: nd array
+        the coordinates timestream of the pointing of each pixel, in degrees
+    """ 
+    pixel_path = []
+    for pixel in pixel_offset:   
+        pixel_w_time = np.append( pixel*np.sin(theta), pixel*np.cos(theta),)
+#         print(pixel_w_time)
+        pixel_path.append(pointing_path+pixel_w_time) 
+        
+    return pixel_path
+
+def genPointingPath(T, scan_path, HA, lat, dec):
+    """
+    Function that takes local paths and generates the pointing on sky vs time.
+    Parameters
+    ----------
+    T: array
+        coordinates timestream of the pointing
+    pixel_offset: float
+        spatial distance between adjacent pixels in degrees
+    theta: float
+        angle in degree
+    Returns
+    -------
+    pixel_path: nd array
+        the coordinates timestream of the pointing of each pixel, in degrees
+    """ 
+    alt = elevationAngle(dec,lat,HA)+np.radians(scan_path[:,1])
+    azi = azimuthAngle(dec,lat,HA)+np.radians(scan_path[:,0])
+    
+    dec_point = declinationAngle(np.degrees(azi), np.degrees(alt), lat)
+    ha_point  = hourAngle(np.degrees(azi), np.degrees(alt), lat)
+    
+    return np.vstack((np.degrees(ha_point-HA*np.pi/12),np.degrees(dec_point))).T
+
+def binMap(pointing_paths, res=0.02, f_range=1,dec=0):
+    
+    """
+    Binning the pointing into 2d array
+    Parameters
+    ----------
+    pointing_paths: 2d array
+        coordinates timestream of the pointing
+    res: float
+        spatial resolution of the map
+    f_range: float
+        range
+    Returns
+    -------
+    xedges: array
+        the x edges of the binned hitmap
+    yedges: array
+        the y edges of the binned hitmap
+    hit_map: 2d array
+        2d histogram of hit on the sky
+
+    """ 
+    x_range = f_range
+    y_range = x_range
+
+    x_res = res
+    y_res = x_res
+
+    xedges = np.arange(-x_range, x_range+x_res, x_res)
+    yedges = dec+np.arange(-y_range, y_range+y_res, y_res)
+
+    pointings = np.concatenate([pixel for pixel in pointing_paths])
+    hit_map   = bining(xedges,yedges, pointings)
+    return xedges,yedges,hit_map
+
+def bining(xedges,yedges,pointings):
+    """
+    Binning the pointing into 2d array
+    Parameters
+    ----------
+    xedges: array
+        the x edges of the binned hitmap
+    yedges: array
+        the y edges of the binned hitmap
+    pointing: 2d array
+        coordinates timestream of the pointing
+    Returns
+    -------
+    H: 2d array
+        2d histogram of hit on the sky
+    """ 
+
+    H, xedges, yedges = np.histogram2d(pointings[:,0], pointings[:,1], bins=(xedges, yedges))
+    return H.T
+
+def az_scan_custom(stripe_size, step_y, num_steps, xoffset=0, yoffset=0, plot=False):
+
+    """
+    Generate the pointing path of a constant elevation scan 
+    Parameters
+    ----------
+    stripe_size: float
+        the size in degrees of one strip
+    step_y: float
+        the elevation step between two stripes in degrees
+    num_step: float
+        the number f step to do
+    xoffet: float
+        the x offset of the beginning of the scan path wrt. the origin, in degree
+    yoffet: float
+        the y offset of the beginning of the scan path wrt. the origin, in degree
+    Returns
+    -------
+    x_series: array
+        azimuth coordinates of the scan path
+    y_series: array
+        elevation coordinates of the scan path
+    """ 
+
+    # Define the field size
+    X = stripe_size / 2  # Half the field size (square bounds)
+
+    # Adjust the starting point and stripe size
+    x0 = -stripe_size / 2
+    
+    # Calculate the y0 offset to center the middle row at y = 0
+    if num_steps % 2 == 1:  # Odd number of rows
+        y0 = -((num_steps // 2) * step_y)
+    else:  # Even number of rows
+        y0 = -((num_steps // 2 - 0.5) * step_y)
+
+    # Calculate the total number of samples required per row
+    dx = stripe_size / 200  # Set a reasonable resolution for the x-direction
+    lx = stripe_size  # Horizontal extent of the stripe
+    samples_per_row = int(lx / dx)
+
+    # Initialize arrays for x and y coordinates
+    x_series = []
+    y_series = []
+
+    # Generate the scan path row by row
+    for step in range(num_steps):
+        # Calculate y position for this step
+        y_current = y0 + step * step_y
+
+        # Generate x values for this row
+        x_row = x0 + (np.arange(samples_per_row) * dx) % lx
+        # Flip every other row for the zig-zag pattern
+        if step % 2 == 1:
+            x_row = -x_row
+
+        # Append the current row's x and y values
+        x_series.append(x_row)
+        y_series.append(np.full_like(x_row, y_current))
+
+    # Flatten the arrays
+    x_series = np.concatenate(x_series)+xoffset-x0
+    y_series = np.concatenate(y_series)+yoffset-y0
+    if(plot):
+        # Plot the scan path
+        plt.plot(x_series, y_series, c='b', alpha=0.8)
+        plt.xlabel('X (rad)')
+        plt.ylabel('Y (rad)')
+        plt.title('Custom Azimuthal Scan Path')
+        plt.show()
+
+    return x_series, y_series
---
+++ scan_strategy/strategy.py
@@ -0,0 +1,327 @@
+import numpy as np
+import scipy as sp
+import matplotlib.colors as colors
+from matplotlib.pyplot import cm
+
+from scipy.integrate import simps
+from time import time 
+import matplotlib.animation as animation
+from scan_fcts import * 
+from TIM_scan_strategy import *
+import importlib
+from astropy.coordinates import SkyCoord
+from IPython import embed
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+import matplotlib.patches as mpatches
+import matplotlib.markers as mmarkers
+import os
+import astropy.units as u
+import matplotlib
+import scipy.constants as cst
+from IPython import embed
+# Set plot parameters
+
+import pygetdata as gd
+import numpy as np
+from IPython import embed
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from astropy.nddata import Cutout2D
+import matplotlib.pyplot as plt
+from astropy.coordinates import SkyCoord
+import h5py
+import os
+import tracemalloc
+import time
+from progress.bar import Bar
+
+import argparse
+
+'''
+matplotlib.rcParams.update({'font.size': 10})
+matplotlib.rcParams.update({'xtick.direction':'in'})
+matplotlib.rcParams.update({'ytick.direction':'in'})
+matplotlib.rcParams.update({'xtick.top':True})
+matplotlib.rcParams.update({'ytick.right':True})
+matplotlib.rcParams.update({'legend.frameon':False})
+matplotlib.rcParams.update({'lines.dashed_pattern':[5,3]})
+'''
+import pickle
+from astropy.cosmology import FlatLambdaCDM
+# Set cosmology to match Bolshoi-Planck simulation
+cosmo = FlatLambdaCDM(H0=67.8, Om0=0.307, Ob0=0.048, Tcmb0=2.7255, Neff=3.04)
+from astropy.wcs import WCS
+from astropy.coordinates import Angle
+
+from simim.lightcone import LCMaker
+from simim.lightcone import LCHandler
+import simim.instrument as inst
+from simim.map import Gridder
+from astropy. io import fits
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+def load_params(path):
+    """
+    Return as a dictionary the parameters stores in a .par file
+    
+    Parameters
+    ----------
+    path: string
+        name of the .par file       
+    Returns
+    -------
+    params: dictionary
+        dictionary containing the loaded parameters
+    """    
+    file = open(path)
+
+    params = {}
+    for line in file:
+        line = line.strip()
+        if not line.startswith("#"):
+            no_comment = line.split('#')[0]
+            key_value = no_comment.split("=")
+            if len(key_value) == 2:
+                params[key_value[0].strip()] = key_value[1].strip()
+
+    for key in params.keys():
+        params[key] = eval(params[key])
+
+    return params
+
+def save_tod_one_array(tod_file, scan_path_sky, samples, pixel_offset, T, spf=3):
+    """
+    Generate the tod for one array of TIM detectors and save it in the .hdf5 format. 
+
+    Parameters
+    ----------
+    tod_file: string 
+        name of the output hdf5 file     
+    scan_path_sky: nd array
+        coordinates of the pointing path of each detector
+    samples: 2d array
+        TOD amplitudes of each detector
+    pixel_offset: array
+        position of each pixel on the array. 
+    T: array
+        hour angle in degree
+
+    Returns
+    -------
+    H: hdf5
+        save the output hdf5 file.
+    """ 
+
+    H = h5py.File(tod_file, "w")
+
+    for i, (name, coord) in enumerate(zip(('RA', 'DEC'), (scan_path_sky[:,0],scan_path_sky[:,1]))):
+        grp = H.create_group(name)
+        grp.create_dataset('data', data=coord, compression='gzip', compression_opts=9)
+
+    for detector, offset in enumerate(pixel_offset):
+        grp = H.create_group(f'kid{detector}_roachN')
+        grp.create_dataset('data', data=samples[detector,:], compression='gzip', compression_opts=9)
+        grp.create_dataset('spf', data=spf)
+        grp.create_dataset('pixel_offset', data=pixel_offset)
+
+    grp = H.create_group('time')
+    grp.create_dataset('data', data=T, compression='gzip', compression_opts=9)
+    grp.create_dataset('spf', data=spf)
+
+    H.close()
+
+def gen_tod_one_array(simu_sky_path, pixel_offset,pointing_paths, ra, dec, plot=True):
+
+    """
+    Generate the tod for one array of TIM detectors and save it in the .hdf5 format. 
+
+    Parameters
+    ----------
+    simu_sky_path: string
+        path to the sky simulation out of which to generate the timestreams
+    pixel_offset: array
+        pixel position on the array wrt the central pixel, in degrees
+    pointing_paths: 2d array
+        coordinates of the scan path, for the central pixe, in degrees
+    ra: float
+        the ra coordinate of the field center
+    dec: float
+        the dec coordinate of the field center
+
+    Returns
+    -------
+    H: hdf5
+        save the output hdf5 file.
+    """ 
+
+    #Generate the TOD given a simulated sky
+    cube = fits.getdata(simu_sky_path)
+    hdr  = fits.getheader(simu_sky_path)
+    hdr['CRVAL1']=ra
+    hdr['CRVAL2']=dec
+    xbins = np.arange(-0.5, hdr['NAXIS1']+0.5, 1)
+    ybins = np.arange(-0.5, hdr['NAXIS2']+0.5, 1)
+    map = cube[0,:,:]
+    wcs = WCS(hdr, naxis=2) 
+
+    positions_x = np.zeros((len(pixel_offset), len(pointing_paths[0][:,0])))
+    positions_y = np.zeros((len(pixel_offset), len(pointing_paths[0][:,0])))
+    samples = np.zeros((len(pixel_offset), len(pointing_paths[0][:,0])))
+
+    for detector, offset in enumerate(pixel_offset):
+        #data = map_f.sample(position_series + offset, properties=idxs)
+        y_pixel_coords, x_pixel_coords = wcs.world_to_pixel_values(pointing_paths[detector][:,0]+hdr['CRVAL1'], pointing_paths[detector][:,1])    
+        # Round the positions and convert to integer indices
+        x_pixel_coords_rounded = np.round(x_pixel_coords).astype(int)
+        y_pixel_coords_rounded = np.round(y_pixel_coords).astype(int)
+        # Create a mask for positions within bounds
+        valid_mask = (
+            (x_pixel_coords_rounded >= 0) & (x_pixel_coords_rounded < hdr['NAXIS1'] - 1) &  # x within bounds
+            (y_pixel_coords_rounded >= 0) & (y_pixel_coords_rounded < hdr['NAXIS2'] - 1) )   # y within bounds
+        # Initialize the output array with zeros
+        values = np.zeros_like(x_pixel_coords_rounded, dtype=float)
+        # Assign values from the map for valid positions
+        values[valid_mask] = map[x_pixel_coords_rounded[valid_mask], y_pixel_coords_rounded[valid_mask]]
+        samples[detector,:] = np.asarray(values.astype(float))
+        positions_x[detector,:] = x_pixel_coords
+        positions_y[detector,:] = y_pixel_coords
+
+    if(plot):
+        norm, edges = np.histogramdd(sample=(positions_x.ravel(), positions_y.ravel()), bins=(xbins,ybins),  )
+        hist, edges = np.histogramdd(sample=(positions_x.ravel(), positions_y.ravel()),  bins=(xbins,ybins), weights=samples.ravel())
+        hist /= norm
+
+        fig, axs = plt.subplots(1,3, figsize=(12,4), dpi = 200,subplot_kw={'projection': wcs}, sharex=True, sharey=True )
+        imgdec = axs[0].imshow(hist, interpolation='nearest', origin='lower', vmin=map.min(), vmax=map.max(), cmap='cividis' )
+        img = axs[1].imshow(map, interpolation='nearest', origin='lower', vmin=map.min(), vmax=map.max(), cmap='cividis' )
+        count = axs[2].imshow(norm, interpolation='nearest', origin='lower', cmap='binary' )
+
+        for ax in (axs[0], axs[1], axs[2]):
+            lon = ax.coords[0]
+            lat = ax.coords[1]
+            lat.set_major_formatter('d.d')
+            lon.set_major_formatter('d.d')
+            lon.set_axislabel('RA')
+            lat.set_axislabel('Dec')
+            if(ax is not axs[0]): ax.tick_params(axis='y', labelleft=False)
+
+        plt.subplots_adjust(wspace=0, hspace=0)
+        plt.show()
+
+    return positions_y, positions_x, samples
+
+if __name__ == "__main__":
+    '''
+    To run: python strategy.py params_strategy.par
+    Left to be done:
+        Add the second array of detectors
+        Produce spectral TOD (One frequency so far)
+        Compute mapping efficiency
+        Get the right acquisition rate
+        Make cst elevation scan decelerate and turn instead of vertical ascenscion 
+        set properly the spf 
+        add noise and atmosphere in TOD
+        make the simIM version
+    '''
+
+    #------------------------------------------------------------------------------------------
+    #load the .par file parameters
+    parser = argparse.ArgumentParser(description="strategy parameters",
+                                     formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+    #options
+    parser.add_argument('params', help=".par file with params", default = None)
+    args = parser.parse_args()
+    P = load_params(args.params)
+    #------------------------------------------------------------------------------------------
+    #Initiate the parameters
+
+    #The coordinates of the field
+    name=P['name_field']
+    c=SkyCoord.from_name(name)
+    ra = c.ra.value
+    dec = c.dec.value
+    #The contour of the field
+    contours = P['contours']
+    x_cen, y_cen = np.mean(contours[:, 1]), np.mean(contours[:, 0])
+    lat = P['latittude']
+    f_range = P['f_range']
+
+    #Load the resolution. 
+    #if not in params, load it from the map used to generate the TOD. 
+    res = P['res']
+    if(res is None):
+        hdr = fits.getheader(P['path']+P['file'])
+        res = (hdr['CDELT1'] * u.Unit(hdr['CUNIT1'])).to(u.deg).value
+
+    #Scan parameters
+    stripe_size = P['stripe_size'] #the lenght of one constant elevation strip in degree
+    dy = P['dy'] #the step in elevation between two consecutive constant elevation strip in degree
+    nb_rows = P['nb_rows'] #the number of step to be done
+    T_duration = P['T_duration'] * 3600 #second angle, duration of the scan.
+    dt = P['dt']*np.pi/3.14 #second angle, such as dt is not rational. Aquisition rate. 
+
+    theta = np.radians(P['theta'])
+
+    #Pixel offsets from the center of the field of view in degree. 
+    pixel_offset_HW = pixelOffset(P['nb_pixel_HW'], P['offset_HW']) 
+    pixel_offset_LW = pixelOffset(P['nb_pixel_LW'], P['offset_LW']) 
+    pixel_offset = pixel_offset_HW
+    #----------------------------------------
+    #Generate the sacan path
+    T = np.arange(0,T_duration,dt)
+    HA = np.arange(-T_duration/2,T_duration/2,dt) / 3600 # hours angle
+    az, alt = az_scan_custom(np.radians(stripe_size),np.radians(dy),nb_rows)
+    az = np.degrees(az); alt=np.degrees(alt)
+    scan_path, _  = genScanPath(T, alt, az, np.zeros(len(alt)))   
+    scan_path_sky = genPointingPath(T, scan_path, HA, lat, dec)
+    pixel_paths  = genPixelPath(scan_path, pixel_offset, theta)
+    pointing_paths = [genPointingPath(T, pixel_path, HA, lat, dec) for pixel_path in pixel_paths]
+    xedges,yedges,hit_map = binMap(pointing_paths,res=res,f_range=f_range,dec=dec,)
+    #----------------------------------------
+    #scan route
+    BS = 10; plt.rc('font', size=BS); plt.rc('axes', titlesize=BS); plt.rc('axes', labelsize=BS); mk = 3; lw=1
+    fig, axs = plt.subplots(2,2,figsize=(10,10), dpi=160,)# sharey=True, sharex=True)
+    axradec, ax, axr, axp = axs[0,0], axs[0,1], axs[1,0], axs[1,1]
+    axradec.plot(az-az.max()/2,alt-alt.max()/2,'cyan')
+    if(contours is not None): axradec.plot(contours[:, 1]-ra, contours[:, 0]-dec, c='g' )
+    axradec.set_xlabel('Az [deg]')
+    axradec.set_ylabel('El [deg]')
+    axradec.set_aspect(aspect=1)
+    if(contours is not None): heatmap, xedges, yedges = np.histogram2d(scan_path_sky[:,0]+contours[:, 1].mean(), scan_path_sky[:,1], bins=int(2/res))
+    else: heatmap, xedges, yedges = np.histogram2d(scan_path_sky[:,0], scan_path_sky[:,1], bins=int(2/res))
+    im = ax.imshow(heatmap.T, origin='lower', cmap='binary',extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
+    cbar = fig.colorbar(im, ax=ax, label='1 detector counts')
+    if(contours is not None): ax.plot(contours[:, 1], contours[:, 0], c='g' )
+    img = axr.imshow((hit_map), extent=[x_cen-f_range, x_cen+f_range,y_cen-f_range, y_cen+f_range,], 
+                    interpolation='nearest', origin='lower', vmin=0, vmax=np.max(hit_map), cmap='binary' )
+    if(contours is not None): axr.plot(contours[:, 1], contours[:, 0], c= 'r')
+    fig.colorbar(img, ax=axr, label='Counts')
+    CS = axr.contour(np.roll(np.sqrt(hit_map*np.roll(hit_map, 4, axis=1)),-2,axis=1), levels=[0.5*np.max(hit_map)], lw=0.5, origin='lower', linewidths=0.6,
+                extent=[x_cen-f_range, x_cen+f_range,y_cen-f_range, y_cen+f_range,], colors='magenta')
+    # CS = ax1.contour(hit_map, levels=[0.5*np.max(hit_map)], lw=0.5, origin='lower', linewidths=0.6,\
+    #             extent=[x_cen-maps, x_cen+maps,y_cen-maps, y_cen+maps,], colors='w')
+    ax.clabel(CS, inline=1, fontsize=8, )
+    #pixel_xy = np.array([-1*pixel_offset*np.sin(theta)-0.036*np.cos(theta), pixel_offset*np.cos(theta)-0.036*np.sin(theta)])
+    pixel_xy = np.array([-1*pixel_offset*np.sin(theta)+0.036*np.cos(theta), pixel_offset*np.cos(theta)+0.072*np.sin(theta)])
+    axr.scatter(pixel_xy[0]+x_cen, pixel_xy[1]+y_cen, s=2, marker='+', c='white',label='SW Spatial Pixels')
+    patchs = []
+    axp.set_title('Pointing Path per pixel')
+    idx = np.arange(len(pixel_paths))[::10]
+    n = 11
+    for i,c in zip(idx, cm.rainbow(np.linspace(0.,1,len(idx)))):
+        axp.scatter(pointing_paths[i][::n,0]+ra, pointing_paths[i][::n,1], s=0.1,c=c)
+        patch = mpatches.Patch(color=c, label= 'pixel %d'%i); patchs.append(patch); 
+    axp.set_aspect(aspect=1)
+    axp.legend(handles=patchs,frameon=False, bbox_to_anchor=(1,1))
+    fig.tight_layout()
+    plt.savefig("scan_route.png")
+    plt.show()
+    #----------------------------------------
+    #Generate the TOds and save it in hdf5
+    simu_sky_path = P['path']+P['file']
+    positions_y, positions_x, samples = gen_tod_one_array(simu_sky_path, pixel_offset,pointing_paths, ra, dec)
+    tod_file=P['path']+'TOD_'+P['file'][:-5]+'.hdf5'
+    save_tod_one_array(tod_file, scan_path_sky, samples, pixel_offset, T, spf=P['spf'])
+    #----------------------------------------
