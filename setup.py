fb1a508889398169b911f1dbd5687a81e7c84388
sriniraghuna <sriniraghuna@gmail.com>
2025-04-25 09:41:15 -0500

    readthedocs stuff

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='TIM_analysis',
+     author='TIM science team',
+      zip_safe=False,
+      install_requires=[
+          'numpy',
+          # 'Sphinx',
+          ],
+      )
+
