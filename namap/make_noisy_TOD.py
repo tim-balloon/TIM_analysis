0699b0aea4868ffde627ef4b8156dd779b49043d
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-04-27 15:52:56 -0500

    update

--- namap/make_noisy_TOD.py
+++ namap/make_noisy_TOD.py
@@ -23,6 +23,28 @@ matplotlib.use("Agg")
 
 
 def gaussian_random_tod(l, clt, nx, res, l_cutoff=None):
+
+    """
+    Generate a gaussian random noise timestream from a power spectrum.  
+
+    Parameters
+    ----------
+    l:  array
+        wavenumber array
+    clt: array
+        power spectrum amplitude array
+    nx: int
+        the number of elements of the timestream
+    res: float
+        the (time) resolution
+    l_cutoff: float
+        maximum wavenumber used in the generation of the timestream. 
+
+    Returns
+    -------
+    real_space_tod: array
+        the generated timestream. 
+    """ 
     
     lmap_rad_x = nx*res
     #Generate gaussian amplitudes
@@ -65,6 +87,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     P = load_params(args.params)
     #------------------------------------------------------------------------------------------
+
+    #------------------------------------------------------------------------------------------
     #Initiate the parameters
     
     #Load the scan duration and generate the time coordinates with the desired acquisition rate. 
@@ -87,7 +111,7 @@ if __name__ == "__main__":
 
     det_names_dict = pd.read_csv(P['detectors_name_file'], sep='\t')
     det_dict = det_names_dict.groupby('Frequency')['Name'].first().to_dict()
-    det_dict = dict(islice(det_dict.items(), 5))
+    #Each pixel with the same offset sees the same beam, but in different frequency band. 
     same_offset_groups = det_names_dict.groupby(['XEL', 'EL'])['Name'].apply(list).reset_index()
     
     tod_file=os.getcwd()+'/'+P['path']+'TOD_'+P['file'][:-5]+'.hdf5'
@@ -97,18 +121,20 @@ if __name__ == "__main__":
     fknee = P['fknee']  ##0.05 #Hz (Fig. 11 of https://arxiv.org/pdf/2106.11202).
     alphaknee = P['alphaknee'] 
     rho_one_over_f = P['rho_one_over_f']  #some level of 1/f correlation between detectors.
+    #------------------------------------------------------------------------------------------
 
-    #----------------------
-    #Load the sky simulation to generate the TOD from
+    ''' 
+    # #Load the WCS
     dwcs = pickle.load( open(P['wcs_dict'], 'rb'))
     wcs = dwcs['wcs']
     #y_pixel_coords, x_pixel_coords = wcs.world_to_pixel_values(ra,dec)    
     xbins = np.arange(-0.5, wcs.pixel_shape[0]+0.5, 1)
     ybins = np.arange(-0.5, wcs.pixel_shape[1]+0.5, 1)
     #norm, edges = np.histogramdd(sample=(x_pixel_coords, y_pixel_coords), bins=(xbins,ybins),)
+    '''
 
-    #---------------------------------------------------
 
+    #For each group of pixels seeing the same beam: 
     for group in range(len(same_offset_groups)):
         start = time.time()
         
@@ -126,7 +152,8 @@ if __name__ == "__main__":
         axs[0,1].set_xlim(1e-2, 1e1)
         axs[0,1].set_ylim(1e-18,1e-5)
         '''
-
+        #------------------------------------------------------------------
+        #Load the sky timestreams (from strategy.py)
         H = h5py.File(tod_file, "a")
         for id, d in enumerate(same_offset_groups.iloc[group]['Name']): 
             #if(id>2): continue
@@ -139,13 +166,18 @@ if __name__ == "__main__":
             #axs[0,1].loglog(freq_fft[inds],curr_spec[inds], alpha=0.1)
             fft_sky_tod.append(curr_spec)
         H.close()
+        #------------------------------------------------------------------
 
+
+        #------------------------------------------------------------------
         #define some detectors
         total_detectors = len(fft_sky_tod)
         detector_array = np.arange( total_detectors )
         detector_arr_to_plot = np.asarray( detector_array )
+        #------------------------------------------------------------------
 
-        #example noise model for auto- and cross-power between detectors
+        #------------------------------------------------------------------
+        #Generate the noise model for auto- and cross-power between detectors
         freq, noise_powspec, noise_powspec_one_over_f, noise_powspec_white = sim_tools_tod.detector_noise_model(tod_noise_level, fknee, alphaknee, tod_len, sample_freq)
         cross_noise_powspec = sim_tools_tod.get_correlated_powspec(rho_one_over_f, noise_powspec_one_over_f, noise_powspec_one_over_f)
         noise_powspec_dic = {}
@@ -155,6 +187,7 @@ if __name__ == "__main__":
                     noise_powspec_dic[i, j] = noise_powspec
                 else:
                     noise_powspec_dic[i, j] = cross_noise_powspec
+        #------------------------------------------------------------------
         '''
         axs[0,1].loglog( freq, noise_powspec, label = r'Total', color = 'black' )
         axs[0,1].loglog( freq, noise_powspec_one_over_f, label = r'$1/f$', color = 'orangered' )
@@ -176,30 +209,34 @@ if __name__ == "__main__":
         axs[1,1].set_ylim(1e-18,1e-5)
         axs[1,1].set_xlabel('frequency [Hz]')
 
-
         axs[0,2].set_title('noise TOD')
         axs[0,2].set_xlabel('$\\rm t_{int}$ [h]')
         axs[0,2].set_ylabel('$\\rm S_{\\nu}$ [Jy]')
         axs[1,2].set_title('noisy TOD')
         axs[1,2].set_xlabel('$\\rm t_{int}$ [h]')
         axs[1,2].set_ylabel('$\\rm S_{\\nu}$ [Jy]')
-        
         '''
-        tod_sims_dic = {}
-        pspec_dic_sims = {}
-
+        #------------------------------------------------------------------
+        #Check if all the detectors in the group already have a noise timestream. 
         H = h5py.File(tod_file, "a")
         name = same_offset_groups.iloc[group]['Name'][-1]
         f = H[f'kid_{name}_roach']
         B = ('noise_data' not in f or 'noisy_data' not in f)
-        H.close()
+        H.close() 
+        #------------------------------------------------------------------
+
+        #------------------------------------------------------------------
+        #If the detectors don't all have a noise timestream yet, generate correlated power spectra
         if(B): 
+
+            tod_sims_dic = {}
+            pspec_dic_sims = {}
 
             for sim_no in range( nsims ):
                 bar = Bar('Processing Sim = %s of %s' %(sim_no+1, nsims), max=total_detectors)
+                #Generate un-correlated simulated timestreams
                 tod_sim_arr = sim_tools_flatsky.make_gaussian_realisations(freq_fft, noise_powspec_dic, tod_shape, 1./sample_freq)
-                ###print( tod_sim_arr.shape ); ##sys.exit()
-                #get the sim spectra now.
+                #get the correlated power spectra now.
                 curr_sim_pspec_dic = {}
                 for (cntr1, tod1) in enumerate( tod_sim_arr ):
                     for (cntr2, tod2) in enumerate( tod_sim_arr ):
@@ -209,7 +246,10 @@ if __name__ == "__main__":
                     bar.next()
                 bar.finish
                 pspec_dic_sims[sim_no] = curr_sim_pspec_dic
+        #------------------------------------------------------------------
 
+        #------------------------------------------------------------------
+        #From the power spectra, generate random gaussian TODs and save them. 
         detector_combs_autos   = [[detector, detector] for detector in detector_array]
         detector_combs_crosses = [[detector1, detector2] for detector1 in detector_array for detector2 in detector_array if (detector1!=detector2 and detector1<detector2)]
         detector_combs = detector_combs_autos + detector_combs_crosses
@@ -244,7 +284,8 @@ if __name__ == "__main__":
                 #axs[1,1].loglog(freq_fft[inds], curr_spec_mean[inds], alpha=0.1)
             
             bar.next()
-        
+        #------------------------------------------------------------------
+
         #fig.tight_layout()
         #fig.savefig(f'plot/group_{group}_summary_plot.png')
         #plt.close()
@@ -254,60 +295,6 @@ if __name__ == "__main__":
         H.close()
         end = time.time()
         timing = end - start
+        print('')
         print(f'Generate the TODs of group{group} in {np.round(timing,2)} sec!')
 
-
-    '''
-    
-    
-    #make plots of the input vs output power spectra.
-    if total_detectors > 5:
-        detector_arr_to_plot = detector_arr_to_plot[:5]
-    detector_combs_for_plotting_autos = [[detector, detector] for detector in detector_arr_to_plot]
-    detector_combs_for_plotting_crosses = [[detector1, detector2] for detector1 in detector_arr_to_plot for detector2 in detector_arr_to_plot if (detector1!=detector2 and detector1<detector2)]
-    detector_combs_for_plotting = detector_combs_for_plotting_autos + detector_combs_for_plotting_crosses
-
-    fsval = 12
-    tr, tc = len(detector_arr_to_plot), len(detector_arr_to_plot)
-    sbpl = 1
-    figure(figsize = (tr+6, tc+6))
-    subplots_adjust( hspace = 0.2, wspace = 0.1)
-
-    for d1d2 in detector_combs_for_plotting:
-        d1, d2 = d1d2
-
-        rowval, colval = np.where(detector_arr_to_plot == d1)[0][0], np.where(detector_arr_to_plot == d2)[0][0]
-        ax = subplot2grid((tr, tc), (rowval, colval), yscale = 'log', xscale = 'log')
-
-        #plot sky data
-        if(d1==d2 and d1<=4):  ax.plot(freq_fft[inds] , fft_sky_tod[d1][inds], c='g', alpha=0.1)
-
-
-        curr_theory = noise_powspec_dic[(d1, d2)]
-        #theory
-        plot( freq_fft[inds], curr_theory[inds], color = 'black', label = r'Input', lw = 2., zorder = 100)
-
-        #sims
-        colorval = 'orangered'
-        curr_spec_arr = []
-        for sim_no in pspec_dic_sims:
-            curr_freq, curr_spec = pspec_dic_sims[sim_no][(d1, d2)]
-            curr_spec_arr.append( curr_spec )
-            plot( curr_freq[inds], curr_spec[inds], color = colorval, lw = 0.1, alpha = 0.1)
-        curr_spec_mean = np.mean( curr_spec_arr, axis = 0 )
-        plot( curr_freq[inds], curr_spec_mean[inds], color = colorval, lw = 0.5, label = r'Sim mean')
-
-        xlim(1e-2, 1e1); ylim(1e-18,1e-5)
-        grid(True, lw = 0.2, alpha = 0.2, which = 'both')
-        if rowval == colval:
-            xlabel(r'Freqeuency [Hz]', fontsize = fsval-2)
-            ylabel(r'Power $\mu$K$^{2}$ seconds', fontsize = fsval-2)
-        else:
-            setp(ax.get_xticklabels(), visible=False);# tick_params(axis='y',left='off')
-            setp(ax.get_yticklabels(), visible=False);# tick_params(axis='y',left='off')
-        if rowval == colval and rowval == 0: legend(loc = 1, fontsize = fsval - 4, ncol = 1)
-        title(r'd%s x d%s' %(d1, d2), fontsize = fsval)
-        sbpl += 1
-    plt.show()
-    show()
-    '''
