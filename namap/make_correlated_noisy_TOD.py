dade9a81f3b70f44f395e4f363da3eafd98418c2
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-04-29 14:17:27 -0500

    update

--- namap/make_correlated_noisy_TOD.py
+++ namap/make_correlated_noisy_TOD.py
@@ -43,6 +43,31 @@ def worker_model(grps):
     return (noise_list, name_list)
 
 def make_all_tods_pll(same_offset_groups, T, sample_freq, tod_len, tod_shape, fmin, fmax, nsims, tod_file, tod_noise_level, fknee, alphaknee, rho_one_over_f):
+    '''
+    Parallelization on 24 nodes of the noise TODs generation. One node now process several group of pixels which see the same beam. 
+    Parameters
+    ----------
+    same_offset_groups: pandas.core.frame.DataFrame
+        pixels grouped by the beam they are seeing 
+    T: array
+        the time timestream
+    sample_freq: float
+        the qcquisition rate of T and of the sky TODs. 
+    tod_len: int
+        lenght of the noise TODs
+    tod_shape: list
+        list of the TODs dimensions
+    fmin: float
+        minimum frequency from which to generate the noise TOD. NOT IMPLEMENTED 
+    fmax: float
+        maximum frequency from which to generate the noise TOD. NOT IMPLEMENTED 
+    nsims: int
+        number of simulation to generate. 
+    tod_file: 
+        name of the hdf5 cntaining the sky TODs and in which to save the noise TODs. 
+    tod_noise_level: float
+
+    '''
 
     grps = np.arange(len(same_offset_groups))
     print('start //')
@@ -139,8 +164,6 @@ def gaussian_random_tod(l, clt, nx, res, l_cutoff=None):
     
     return real_space_tod
 
-
-
 def make_correlated_timestreams(total_detectors, T, sample_freq, tod_len, tod_shape, fmin, fmax, nsims, tod_file, tod_noise_level, fknee, alphaknee, rho_one_over_f, plot=False):
 
     '''
@@ -214,13 +237,9 @@ def make_correlated_timestreams(total_detectors, T, sample_freq, tod_len, tod_sh
     #If the detectors don't all have a noise timestream yet, generate correlated power spectra
     tod_sims_dic = {}
     pspec_dic_sims = {}
-
     for sim_no in range( nsims ):
-
         #Generate un-correlated simulated timestreams
         tod_sim_arr = sim_tools_flatsky.make_gaussian_realisations(freq_fft, noise_powspec_dic, tod_shape, 1./sample_freq) 
-                        
-        #bar = Bar('Processing Sim = %s of %s' %(sim_no+1, nsims), max=total_detectors)
         #get the correlated power spectra now.
         curr_sim_pspec_dic = {}
         for (cntr1, tod1) in enumerate( tod_sim_arr ):
@@ -228,9 +247,6 @@ def make_correlated_timestreams(total_detectors, T, sample_freq, tod_len, tod_sh
                 if cntr2<cntr1: continue       
                 curr_spec = ( np.fft.fft(tod1) * (1/sample_freq) * np.conj( np.fft.fft(tod2) * (1/sample_freq) ) / tod_len  ).real
                 curr_sim_pspec_dic[(cntr1, cntr2)] = [freq_fft, curr_spec]
-            #bar.next()
-        #bar.finish              
-        #curr_sim_pspec_dic = model_pll(freq_fft, tod_sim_arr, sample_freq, tod_len, ncpus)
         pspec_dic_sims[sim_no] = curr_sim_pspec_dic
     #------------------------------------------------------------------
 
@@ -248,27 +264,115 @@ def make_correlated_timestreams(total_detectors, T, sample_freq, tod_len, tod_sh
         curr_spec_list.append( np.mean( curr_spec_arr, axis = 0 ) )
 
     noise_tods_list = []
-    if(plot): namap_tods_list = []
     for d1d2 in detector_combs:
         d1, d2 = d1d2
         curr_theory = noise_powspec_dic[(d1, d2)]
         if(d1==d2):
             noise_tod = gaussian_random_tod(freq_fft, curr_spec_list[d1], res = (1/sample_freq), nx = tod_len)
             noise_tods_list.append(noise_tod)
-
-            #data_with_slope = add_polynome_to_timestream(sky_tod[d1], T) + noise_tod
-            #data_with_peaks = add_peaks_to_timestream(data_with_slope)
-            #namap_tods_list.append(data_with_peaks)
-
-            #if('corr_noise_data' in f): del f['corr_noise_data'] 
-            #if('corr_noisy_data' in f): del f['corr_noisy_data'] 
-            #f.create_dataset('corr_noise_data', data=noise_tod, compression='gzip', compression_opts=9)
-            #f.create_dataset('corr_noisy_data', data=tod_tot, compression='gzip', compression_opts=9)
-
-            #if('namap_data' in f): del f['namap_data'] 
-            #f.create_dataset('namap_data', data=data_with_peaks,   compression='gzip', compression_opts=9)
     #------------------------------------------------------------------
+
+    return noise_tods_list
+
+if __name__ == "__main__":
+
+    '''
+    Generate noise TOD. 
+    The noise TOD are frequency, detector, and time independant.
+    The noise TODs are gaussian. 
+    The noise TODs for pixels seeing the same beam (at different frequency bands) are correlated. 
+    '''
+    #------------------------------------------------------------------------------------------
+    #load the .par file parameters
+    parser = argparse.ArgumentParser(description="strategy parameters",
+                                     formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+    #options
+    parser.add_argument('params', help=".par file with params", default = None)
+    args = parser.parse_args()
+    P = load_params(args.params)
+    #------------------------------------------------------------------------------------------
+
+    #------------------------------------------------------------------------------------------
+    #Initiate the parameters
+    pll = True
+    ncpus = 24
     
+    #Load the scan duration and generate the time coordinates with the desired acquisition rate. 
+    T_duration = P['T_duration'] 
+    dt = P['dt']*np.pi/3.14 #Make the timestep non rational to avoid some stripes in the hitmap. 
+    T = np.arange(0,T_duration,dt) * 3600
+
+    #TOD specs
+    sample_freq = 1 / np.round(dt*3600,3)
+    tod_len = len(T)
+    tod_shape = [tod_len]
+    #get params for sim generation
+    fmin = P['fmin']
+    fmax = P['fmax']
+
+    #sim specs
+    nsims = P['nsim_noise']
+
+    det_names_dict = pd.read_csv(P['detectors_name_file'], sep='\t')
+    #Each pixel with the same offset sees the same beam, but in different frequency band. 
+    same_offset_groups = det_names_dict.groupby(['XEL', 'EL'])['Name'].apply(list).reset_index()
+    
+    tod_file=os.getcwd()+'/'+P['path']+'TOD_'+P['file'][:-5]+'.hdf5'
+
+    #rough noise specs - similar to SPT (https://arxiv.org/pdf/2106.11202).
+    tod_noise_level = P['tod_noise_level'] #in, for example, uK/\sqrt(seconds), units. (Fig. 11 of https://arxiv.org/pdf/2106.11202).
+    fknee = P['fknee']  ##0.05 #Hz (Fig. 11 of https://arxiv.org/pdf/2106.11202).
+    alphaknee = P['alphaknee'] 
+    rho_one_over_f = P['rho_one_over_f']  #some level of 1/f correlation between detectors.
+    #------------------------------------------------------------------------------------------
+
+    #Use the // OR the un// version. 
+
+    #------------------------------------------------------------------------------------------
+    #// version 
+    if(pll):
+        start = time.time()
+        make_all_tods_pll(same_offset_groups, T, sample_freq, tod_len, tod_shape, fmin, fmax, nsims, tod_file, tod_noise_level, fknee, alphaknee, rho_one_over_f)
+        end = time.time()
+        timing = end - start
+        print(f'Generate all the TODs in {np.round(timing,2)} sec!')   
+    #------------------------------------------------------------------------------------------
+
+    #------------------------------------------------------------------------------------------
+    #un// version
+    else:         
+        #For each group of pixels seeing the same beam: 
+        for group in range(len(same_offset_groups)):
+
+            start = time.time()
+            tod_list = make_correlated_timestreams(group, same_offset_groups, T, sample_freq, tod_len, tod_shape, fmin, fmax, nsims, tod_file, tod_noise_level, fknee, alphaknee, rho_one_over_f)
+
+            print('saving')
+            H = h5py.File(tod_file, "a")    
+            for j, (tod, name) in  enumerate(zip(tod_list, same_offset_groups.iloc[group]['Name'])):
+                f = H[f'kid_{name}_roach']
+                sky_tod = f['data']
+                data_with_slope = add_polynome_to_timestream(sky_tod, T) + tod
+                data_with_peaks = add_peaks_to_timestream(data_with_slope)
+                if('corr_noise_data' in f): del f['corr_noise_data'] 
+                if('corr_noisy_data' in f): del f['corr_noisy_data'] 
+                f.create_dataset('corr_noise_data', data=tod, compression='gzip', compression_opts=9)
+                f.create_dataset('corr_noisy_data', data=tod+sky_tod, compression='gzip', compression_opts=9)
+                if('namap_data' in f): del f['namap_data'] 
+                f.create_dataset('namap_data', data=data_with_peaks,   compression='gzip', compression_opts=9)
+            H.close()
+        
+            end = time.time()
+            timing = end - start
+            
+            print(f'Generate the TODs of group {group} in {np.round(timing,2)} sec!')
+    #------------------------------------------------------------------
+
+
+
+
+
+'''  
     if(plot):
 
         #------------------------------------------------------------------
@@ -343,99 +447,4 @@ def make_correlated_timestreams(total_detectors, T, sample_freq, tod_len, tod_sh
         fig.tight_layout()
         fig.savefig(f'plot/group_{group}_summary_plot.png')
         plt.close()
-
-    return noise_tods_list
-
-if __name__ == "__main__":
-    '''
-    Generate noise TOD. 
-    The noise TOD are frequency, detector, and time independant.
-    The noise TODs are gaussian. 
-    The noise TODs for pixels seeing the same beam (at different frequency bands) are correlated. 
-    '''
-    #------------------------------------------------------------------------------------------
-    #load the .par file parameters
-    parser = argparse.ArgumentParser(description="strategy parameters",
-                                     formatter_class = argparse.ArgumentDefaultsHelpFormatter)
-    #options
-    parser.add_argument('params', help=".par file with params", default = None)
-    args = parser.parse_args()
-    P = load_params(args.params)
-    #------------------------------------------------------------------------------------------
-
-    #------------------------------------------------------------------------------------------
-    #Initiate the parameters
-    ncpus = 24
-    
-    #Load the scan duration and generate the time coordinates with the desired acquisition rate. 
-    T_duration = P['T_duration'] 
-    dt = P['dt']*np.pi/3.14 #Make the timestep non rational to avoid some stripes in the hitmap. 
-    T = np.arange(0,T_duration,dt) * 3600
-
-    #TOD specs
-    sample_freq = 1 / np.round(dt*3600,3)
-    tod_len = len(T)
-    tod_shape = [tod_len]
-    #get params for sim generation
-    fmin = P['fmin']
-    fmax = P['fmax']
-
-    #sim specs
-    nsims = P['nsim_noise']
-
-    det_names_dict = pd.read_csv(P['detectors_name_file'], sep='\t')
-    #Each pixel with the same offset sees the same beam, but in different frequency band. 
-    same_offset_groups = det_names_dict.groupby(['XEL', 'EL'])['Name'].apply(list).reset_index()
-    
-    tod_file=os.getcwd()+'/'+P['path']+'TOD_'+P['file'][:-5]+'.hdf5'
-
-    #rough noise specs - similar to SPT (https://arxiv.org/pdf/2106.11202).
-    tod_noise_level = P['tod_noise_level'] #in, for example, uK/\sqrt(seconds), units. (Fig. 11 of https://arxiv.org/pdf/2106.11202).
-    fknee = P['fknee']  ##0.05 #Hz (Fig. 11 of https://arxiv.org/pdf/2106.11202).
-    alphaknee = P['alphaknee'] 
-    rho_one_over_f = P['rho_one_over_f']  #some level of 1/f correlation between detectors.
-    #------------------------------------------------------------------------------------------
-
-    #Use the // OR the un// version. 
-
-    #------------------------------------------------------------------------------------------
-    #// version 
-    start = time.time()
-    make_all_tods_pll(same_offset_groups, T, sample_freq, tod_len, tod_shape, fmin, fmax, nsims, tod_file, tod_noise_level, fknee, alphaknee, rho_one_over_f)
-    end = time.time()
-    timing = end - start
-    print(f'Generate all the TODs in {np.round(timing,2)} sec!')   
-    #------------------------------------------------------------------------------------------
-
-    #------------------------------------------------------------------------------------------
-    '''
-    #un// version
-    
-    #For each group of pixels seeing the same beam: 
-    for group in range(len(same_offset_groups)):
-
-        start = time.time()
-        tod_list = make_correlated_timestreams(group, same_offset_groups, T, sample_freq, tod_len, tod_shape, fmin, fmax, nsims, tod_file, tod_noise_level, fknee, alphaknee, rho_one_over_f)
-
-        print('saving')
-        H = h5py.File(tod_file, "a")    
-        for j, (tod, name) in  enumerate(zip(tod_list, same_offset_groups.iloc[group]['Name'])):
-            f = H[f'kid_{name}_roach']
-            sky_tod = f['data']
-            data_with_slope = add_polynome_to_timestream(sky_tod, T) + tod
-            data_with_peaks = add_peaks_to_timestream(data_with_slope)
-            if('corr_noise_data' in f): del f['corr_noise_data'] 
-            if('corr_noisy_data' in f): del f['corr_noisy_data'] 
-            f.create_dataset('corr_noise_data', data=tod, compression='gzip', compression_opts=9)
-            f.create_dataset('corr_noisy_data', data=tod+sky_tod, compression='gzip', compression_opts=9)
-            if('namap_data' in f): del f['namap_data'] 
-            f.create_dataset('namap_data', data=data_with_peaks,   compression='gzip', compression_opts=9)
-        H.close()
-    
-        end = time.time()
-        timing = end - start
-        
-        print(f'Generate the TODs of group {group} in {np.round(timing,2)} sec!')
-    '''
-    #------------------------------------------------------------------
-
+'''
