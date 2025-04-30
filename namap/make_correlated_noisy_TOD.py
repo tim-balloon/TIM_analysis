f3637a2a313313c70846ca553946f58504a76850
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-04-30 17:26:30 -0500

    udapte

--- namap/make_correlated_noisy_TOD.py
+++ namap/make_correlated_noisy_TOD.py
@@ -380,7 +380,7 @@ if __name__ == "__main__":
         for group in range(len(same_offset_groups)):
 
             start = time.time()
-            total_detectors = 2 #len(same_offset_groups.iloc[group]['Name'])
+            total_detectors = len(same_offset_groups.iloc[group]['Name'])
             tod_list = make_correlated_timestreams(total_detectors, T, sample_freq, tod_len, tod_shape, fmin, fmax, nsims, tod_file, tod_noise_level, fknee, alphaknee, rho_one_over_f)
 
             print('saving')
