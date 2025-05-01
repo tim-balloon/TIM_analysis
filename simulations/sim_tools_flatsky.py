3149aef0f47d73c0a6b0ad2a13188c500df4bf72
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-05-01 09:17:45 -0500

    capture warning

--- simulations/sim_tools_flatsky.py
+++ simulations/sim_tools_flatsky.py
@@ -7,6 +7,10 @@ generating correlated Gaussian realisations across different bands.
 
 import numpy as np, sys, os, warnings
 
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+
 def get_lxly(map_shape, pixel_res_radians):
 
     """
