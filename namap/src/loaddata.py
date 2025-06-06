af020cee24057307739d387c41ebfd9429549043
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-06-06 13:14:20 -0500

    add src

---
+++ namap/src/loaddata.py
@@ -0,0 +1,276 @@
+#import pygetdata as gd
+import numpy as np
+from scipy.interpolate import interp1d
+import os
+import astropy.table as tb
+from IPython import embed
+import src.detector as det 
+import h5py
+import matplotlib.pyplot as plt
+
+def load_params(path):
+    """
+    Return as a dictionary the parameters stores in a .par file
+    
+    Parameters
+    ----------
+    path: string
+        name of the .par file       
+    Returns
+    -------
+    params: dictionary
+        dictionary containing the loaded parameters
+    """    
+    file = open(path)
+
+    params = {}
+    for line in file:
+        line = line.strip()
+        if not line.startswith("#"):
+            no_comment = line.split('#')[0]
+            key_value = no_comment.split("=")
+            if len(key_value) == 2:
+                params[key_value[0].strip()] = key_value[1].strip()
+
+    for key in params.keys():
+        params[key] = eval(params[key])
+
+    return params
+
+class data_value():
+    
+    '''
+    Class for reading the values of the TODs (detectors and coordinates) from a DIRFILE
+    Parameters
+    ----------
+    Returns
+    -------
+    '''
+
+    def __init__(self, det_path, det_name, coord1_name, \
+                 coord2_name, startframe, numframes, telemetry=False):
+
+        """
+        Class to load the data from a .hdf5 
+        Parameters
+        ----------
+        det_path: string
+            Path of the .hdf5
+        det_name: string
+            Detector names to be analyzed
+        coord1_name: string
+            Coordinates 1 name, e.g. RA or AZ
+        coord2_name: string
+            Coordinates 2 name
+        startframe:
+            Starting frame to be analyzed
+        numframes:
+            Ending frame to be analyzed
+
+        Returns
+        -------
+        self: class
+            Instance of the data_value class
+        """    
+        self.det_path = det_path                    #Path of the detector dirfile
+        self.det_name = det_name                    #Detector name to be analyzed
+        self.coord1_name = coord1_name              #Coordinates 1 name, e.g. RA or AZ
+        self.coord2_name = coord2_name              #Coordinates 2 name
+        self.startframe = startframe        #Starting frame to be analyzed
+        self.numframes = numframes           #Ending frame to be analyzed
+
+        if self.startframe < 100:
+            self.bufferframe = int(0)  #Buffer frames to be loaded before and after the starting and ending frame
+        else:
+            self.bufferframe = int(100)
+
+        self.telemetry = telemetry
+ 
+    def loadspf(file, field):
+        """
+        Load the sample per frame of a field from a .hdf5 
+        Parameters
+        ----------
+        file: string
+            the name of the .hdf5 file
+        field: string
+            the field for which to get the spf
+        Returns
+        -------
+        spf: int
+            number of sample per frame
+        """    
+        H = h5py.File(file, "a")
+        f = H[field]
+        if('spf' in f.keys()): spf = f['spf'][()]
+        else: spf = None
+        H.close()
+        return spf
+    
+    def loaddata(file, field, num_frames=None, first_frame=None):
+        """
+        Load the data from a .hdf5 
+        Equivalent to d.getdata()
+        Parameters
+        ----------
+        file: string
+            the name of the .hdf5 file
+        field: string
+            the field to be loaded
+        num_frame: int
+            the number of frames to load, with N=spf samples in each frame.
+        first_frame: int
+            the first frame to load. 
+
+        Returns
+        -------
+        data: array
+            values stores in field, from first_frame*spf to (first_frame+num_frames)*spf. 
+        """    
+
+        if os.path.isfile(file): H = h5py.File(file, "a")
+        else: print('no file')
+        f = H[field]
+        if(('spf' in f.keys()) and (num_frames is not None) and (first_frame is not None)):
+            spf = f['spf'][()]
+            #if(not 'roach' in field): data = f['data'][first_frame*spf:(first_frame+num_frames)*spf]
+            data = f['data'][first_frame*spf:(first_frame+num_frames)*spf]
+        else: 
+            data = f['data'][()]
+        H.close()
+        return data
+
+    def values(self):
+        """      
+        Load the RA, Dec, and amplitudes timestreams for a given list of detectors
+        Parameters
+        ----------
+        Returns
+        -------
+        det_data: list
+            list of amplitude timestreams
+        coord1_data: array
+            Coord 1 timestream
+        coord2_data: array
+            Coord 2 timestream
+        lst: array
+            local sideral time timestream
+        lat: array
+            latitude timestream of a detector
+        spf_data: int
+            the number of sample per frame of the amplitude timestreams. 
+        spf_coord: int
+            the number of sample per frame of the coordinates timestreams
+        lst_lat_spf: int
+            the number of sample per frame of lat and lst. 
+
+        """    
+        num = self.numframes+self.bufferframe*2
+        first_frame = self.startframe+self.bufferframe
+        kid_num  = self.det_name
+
+        det_data = []
+
+        for kid in kid_num: 
+            kidutils = det.kidsutils()
+            det_data.append( data_value.loaddata(self.det_path, f'kid_{kid}_roach', num, first_frame) ) #kidutils.KIDmag(I_data, Q_data))
+            # Assume all the data have the same spf            
+            spf_data = data_value.loadspf(self.det_path, f'kid_{kid}_roach')
+            #---------------------------------------------------------------------------------
+
+        coord1_data = data_value.loaddata(self.det_path, f'{self.coord1_name}', num, first_frame) 
+        coord2_data = data_value.loaddata(self.det_path, f'{self.coord2_name}', num, first_frame) 
+        spf_coord = data_value.loadspf(self.det_path, self.coord2_name, )
+        #---------------------------------------------------------------------------------
+        lat = data_value.loaddata(self.det_path, 'lat',num, first_frame)
+        lst = data_value.loaddata(self.det_path, 'lst',num, first_frame)
+        lst_lat_spf = data_value.loadspf(self.det_path, 'lst')
+
+        return det_data, coord1_data, coord2_data, lst, lat, spf_data, spf_coord, lst_lat_spf
+
+class xsc_offset():
+    """
+    class to read star camera offset files
+    Parameters
+    ----------
+    Returns
+    -------
+    """    
+    def __init__(self, xsc, frame1, frame2):
+
+        self.xsc = xsc #Star Camera number
+        self.frame1 = frame1 #Starting frame
+        self.frame2 = frame2 #Ending frame
+
+    def read_file(self):
+
+        '''
+        Function to read a star camera offset file and return the coordinates 
+        offset
+        Parameters
+        ----------
+        Returns
+        -------
+        '''
+
+        path = os.getcwd()+'/xsc_'+str(int(self.xsc))+'.txt'
+
+        xsc_file = np.loadtxt(path, skiprows = 2)
+
+        index, = np.where((xsc_file[0]>=float(self.frame1)) & (xsc_file[1]<float(self.frame2)))
+
+        if np.size(index) > 1:
+            index = index[0]
+
+        return xsc_file[2], xsc_file[3]
+
+class det_table():
+
+    '''
+    Class to read detector tables.
+    Parameters
+    ----------
+    Returns
+    -------
+    '''
+
+    def __init__(self, dets, pathtable):
+
+        self.name = dets
+        self.pathtable = pathtable
+
+    def loadtable(self):
+        '''
+        Function to load the detectors info from the decteror file. 
+        Parameters
+        ----------
+        Returns
+        -------
+        det_off: list
+            list of angular offsets from the center of the array for each detectors. 
+        noise: array
+            list of detectors white noise.
+        resp: array
+            list of detectors response. 
+        '''
+
+        det_off = np.zeros((np.size(self.name), 2))
+        noise = np.ones(np.size(self.name))
+        resp = np.zeros(np.size(self.name))
+
+        path = self.pathtable
+        btable = tb.Table.read(path, format='ascii.tab')
+
+        for i, kid in enumerate(self.name):
+
+            index, = np.where(btable['Name'] == kid)
+            det_off[i, 0] = btable['XEL'][index] 
+            det_off[i, 1] = btable['EL'][index] 
+
+            noise[i] = btable['WhiteNoise'][index]
+            resp[i] = btable['Resp.'][index]#*-1.
+
+
+        return det_off, noise, resp
+
+ 
---
+++ namap/src/mapmaker.py
@@ -0,0 +1,355 @@
+import numpy as np
+from astropy import wcs
+from astropy.convolution import Gaussian2DKernel, convolve
+from IPython import embed
+import matplotlib.pyplot as plt
+import os
+from astropy.io import fits
+import datetime
+
+class maps():
+
+    '''
+    Wrapper class for the wcs_word class and the mapmaking class.
+    Parameters
+    ----------
+    Returns
+    -------
+    '''
+
+    def __init__(self, ctype, crpix, cdelt, crval, pixnum, data, coord1, coord2, convolution, std, coadd=False, noise=1., telcoord=False, parang=None):
+        '''
+        Create an instance of maps
+        Parameters
+        ----------
+        Returns
+        -------
+        '''
+
+        self.ctype = ctype             #see wcs_world for explanation of this parameter
+        self.crpix = crpix             #see wcs_world for explanation of this parameter
+        self.cdelt = cdelt             #see wcs_world for explanation of this parameter
+        self.crval = crval             #see wcs_world for explanation of this parameter
+        self.pixnum = pixnum           #Max number of pixel
+        self.coord1 = coord1           #array of the first coordinate
+        self.coord2 = coord2           #array of the second coordinate
+        self.data = data               #cleaned TOD that is used to create a map
+        self.w = 0.                    #initialization of the coordinates of the map in pixel coordinates
+        self.proj = 0.                 #inizialization of the wcs of the map. see wcs_world for more explanation about projections
+        self.convolution = convolution #parameters to check if the convolution is required
+        self.std = float(std)          #std of the gaussian is the convolution is required
+        self.noise = noise             #white level noise of detector(s)
+        self.telcoord = telcoord       #If True the map is drawn in telescope coordinates. That means that the projected plane is rotated
+        self.coadd = coadd       #If to coadd all the detectors maps or return their individual maps. 
+        if parang is not None:
+            self.parang = [np.radians(p) for p in parang ] #Parallactic Angle. This is used to compute the pixel indices in telescopes coordinates
+        else:
+            self.parang = parang
+
+    def wcs_proj(self):
+
+        '''
+        Function to compute the projection and the pixel coordinates
+        Parameters
+        ----------
+        Returns
+        -------
+        '''
+        wcsworld = wcs_world(self.ctype, self.crpix, self.cdelt, self.crval, self.telcoord)
+        proj, w = wcsworld.world(self.coord1,self.coord2, self.parang)
+        self.proj = proj
+        self.w = w
+
+    def map2d(self):
+
+        '''
+        Function to generate the maps using the pixel coordinates to bin
+        Parameters
+        ----------
+        Returns
+        -------
+        '''
+        mapmaker = mapmaking(self.data, self.noise, len(self.data), self.proj, self.coadd)
+        Pow_map = mapmaker.map_Ionly(coadd=self.coadd)
+
+        if not self.convolution: return Pow_map
+        else:
+            #Needs to be modify ! 
+            std_pixel = self.std/3600./self.cdelt[0]
+            return mapmaker.convolution(std_pixel, Pow_map)
+        
+    def map_plot(self, data_maps, kid_num):
+
+        """
+        Plot the map out of the data timestreams.     
+        Parameters
+        ---------- 
+        data_maps: list
+            list of maps to plot
+        kid_num: list: 
+            names of the kids used to generate the list of maps.   
+        Returns
+        -------
+        """    
+        crval = self.w.wcs.crval
+        cdelt = self.w.wcs.cdelt[0]
+        ctype = self.ctype
+        pixnum=self.pixnum
+
+
+        xform ='d.ddd'
+        yform ='d.ddd'
+        if self.telcoord is False or True:
+            if ctype == 'RA and DEC':
+                xlab = 'RA (deg)'
+                ylab = 'Dec (deg)'
+            
+            elif ctype == 'AZ and EL':
+                xlab = 'AZ (deg)'
+                ylab = 'EL (deg)'
+            
+            elif ctype == 'CROSS-EL and EL':
+                xlab = 'xEL (deg)'
+                ylab = 'EL (deg)'
+
+            elif ctype == 'XY Stage':
+                xlab = 'X'
+                ylab = 'Y'
+        else:
+            xlab = 'YAW (deg)'
+            ylab = 'PITCH (deg)'
+
+
+        if(self.coadd):
+            fig, ax = plt.subplots(dpi=150, subplot_kw={'projection': self.w})
+            im = ax.imshow(data_maps, origin='lower', interpolation='None', cmap='cividis' )
+                        
+            cbar = fig.colorbar(im, ax=ax, orientation='vertical',)
+            cbar.set_label('Intensity')  # Adjust the label if needed
+
+            ax.set_title('Coadd Map')
+
+            xel = ax.coords[0]
+            el = ax.coords[1]
+            xel.set_axislabel(xlab)
+            el.set_axislabel(ylab)
+            
+            plt.tight_layout()
+            path = os.getcwd()+'/plot/'+f'coadd.png'
+            plt.savefig(path, transparent=True)
+            #plt.show()
+            
+            f = fits.PrimaryHDU(data_maps, header=self.w.to_header())
+            hdu = fits.HDUList([f])
+            hdr = hdu[0].header
+            hdr.set("map")
+            hdr.set("Datas")
+            hdr["BITPIX"] = ("64", "array data type")
+            hdr["BUNIT"] = 'MJy/sr'
+            hdr["DATE"] = (str(datetime.datetime.now()), "date of creation")
+            hdu.writeto( os.getcwd()+'/fits_and_hdf5/'+f'coadd.fits', overwrite=True)
+            hdu.close()
+
+        else: 
+            for m, name in zip(data_maps, kid_num): 
+
+                fig, ax = plt.subplots(dpi=150, subplot_kw={'projection': self.w})
+                im = ax.imshow(m, origin='lower', interpolation='None', cmap='cividis' )
+                cbar = fig.colorbar(im, ax=ax, orientation='vertical', fraction=0.046, pad=0.04)
+                cbar.set_label('Intensity')  # Adjust the label if needed
+                ax.set_title(f'Map of {name}')
+
+                xel = ax.coords[0]
+                el = ax.coords[1]
+                xel.set_axislabel(xlab)
+                el.set_axislabel(ylab)
+
+                plt.tight_layout()
+                path = os.getcwd()+'/plot/'+f'{name}.png'
+                plt.savefig(path, transparent=True)
+                #if(len(kid_num)<6): plt.show()
+                #else: plt.close()
+                plt.show()
+
+                f = fits.PrimaryHDU(m, header=self.w.to_header())
+                hdu = fits.HDUList([f])
+                hdr = hdu[0].header
+                hdr.set("map")
+                hdr.set("Datas")
+                hdr["BITPIX"] = ("64", "array data type")
+                hdr["BUNIT"] = 'MJy/sr'
+                hdr["DATE"] = (str(datetime.datetime.now()), "date of creation")
+                hdu.writeto(os.getcwd()+'/fits_and_hdf5/'+f'map_{name}.fits', overwrite=True)
+                hdu.close()
+
+class wcs_world():
+
+    '''
+    Class to generate a wcs using astropy routines.
+
+    Parameters
+    ----------
+    Returns
+    -------
+    '''
+    def __init__(self, ctype, crpix, cdelt, crval, telcoord=False):
+
+        self.ctype = ctype    #ctype of the map, which projection is used to convert coordinates to pixel numbers
+        self.cdelt = cdelt  #cdelt of the map, distance in deg between two close pixels
+        self.crpix = crpix    #crpix of the map, central pixel of the map in pixel coordinates
+        self.crval = crval    #crval of the map, central pixel of the map in sky/telescope (depending on the system) coordinates
+        self.telcoord = telcoord #Telescope coordinates boolean value. Check map class for more explanation
+
+    def world(self, coord1, coord2, parang): 
+        
+        '''
+        Function for creating a wcs projection and a pixel coordinates 
+        from sky/telescope coordinates
+        Parameters
+        ----------
+        coord1: list
+            list of timestreams of sky coordinates 1 
+        coord2: list
+            list of timestreams of sky coordinates 2
+        parang: array
+            list of parallactic angle in degree. 
+        Returns
+        -------
+        world: list
+            pixel projection of coord1 and coord2 given w
+        w: wcs object
+            the world coordinate system object of Astropy
+        '''        
+
+        w = wcs.WCS(naxis=2)
+        w.wcs.crpix = self.crpix #wo.wcs.crpix
+        w.wcs.cdelt = self.cdelt
+        w.wcs.crval = self.crval
+
+        if self.telcoord is False: w.wcs.ctype = ["TLON-CAR", "TLAT-CAR"]
+        if self.ctype == 'RA and DEC':  w.wcs.ctype = ["RA---TAN", "DEC--TAN"]
+        elif self.ctype == 'AZ and EL': w.wcs.ctype = ["TLON-ARC", "TLAT-ARC"]
+        elif self.ctype == 'CROSS-EL and EL': w.wcs.ctype = ["TLON-CAR", "TLAT-CAR"]
+
+        world = []
+        for c1,c2 in zip(coord1, coord2):
+            world.append(  w.world_to_pixel_values(c1,c2) )
+        # w.world_to_pixel_values(pointing_paths[detector][:,0]+hdr['CRVAL1'], pointing_paths[detector][:,1])    
+        
+        return world, w
+
+class mapmaking(object):
+
+    '''
+    Class to generate the maps. 
+    Parameters
+    ----------
+    Returns
+    -------
+    
+    '''
+
+    def __init__(self, data, weight, number, pixelmap, coadd):
+
+        self.data = data               #detector TOD
+        self.weight = weight           #weights associated with the detector values
+        self.number = number           #Number of detectors to be mapped
+        self.pixelmap = pixelmap       #Coordinates of each point in the TOD in pixel coordinates
+        self.coadd = coadd       #If to coadd all the detectors maps or return their individual maps. 
+
+    def map_Ionly(self, coadd=False, value=None, noise=None, pixelmap = None):
+        
+        '''
+        Function to create the 2D map
+        Parameters
+        ----------
+        coadd: bool
+            to return the coadd map between all detectors or the individual maps. 
+        value: list
+            amplitude timestreams of the detectors
+        noise: array
+            list of the noise in the detectors
+        pixelmap: list
+            list of pixel coordinates timestreams of the detectors
+        Returns
+        -------
+        '''
+
+        if value is None: value = self.data.copy()
+        else: value = value
+
+        if pixelmap is None: pixelmap = self.pixelmap.copy()
+        else: pixelmap = pixelmap
+        
+        if noise is None:  noise = self.weight**2
+        else: noise = noise
+
+        import pickle
+
+        Xmin = np.inf
+        Xmax = -np.inf
+        Ymin = np.inf
+        Ymax = -np.inf
+
+        for i in range(self.number):
+            idxpixel = self.pixelmap[i]
+            
+            # Extract min and max for x and y
+            xmin, xmax = idxpixel[0].min(), idxpixel[0].max()
+            ymin, ymax = idxpixel[1].min(), idxpixel[1].max()
+            
+            # Update global min and max
+            Xmin = min(Xmin, xmin)
+            Xmax = max(Xmax, xmax)
+            Ymin = min(Ymin, ymin)
+            Ymax = max(Ymax, ymax)
+            
+        edges = np.round((Xmin, Xmax, Ymin, Ymax))
+        X_edges = np.arange(edges[0]-0.5, edges[1]+1.5,1)        
+        Y_edges = np.arange(edges[2]-0.5, edges[3]+1.5,1)        
+
+        samples = []
+        coord1samples = []
+        coord2samples = []
+        individual_maps = []
+
+        for pix, val, n, i in zip(self.pixelmap, value, noise, range(self.number)):
+            #------
+            if n !=0: sigma = 1/n**2
+            else: sigma = 1
+            #val *= sigma
+            samples.append(val)
+            coord1samples.append(pix[0])
+            coord2samples.append(pix[1])
+            hits, x_edges, y_edges = np.histogram2d(pix[0], pix[1], bins = (X_edges, Y_edges) )
+            flux, x_edges, y_edges = np.histogram2d(pix[0], pix[1], bins = (X_edges, Y_edges), weights=val )
+            w = np.where(hits>0)
+            flux[w] /= hits[w]
+            individual_maps.append(flux.T)
+
+        if not coadd: return individual_maps
+        else: 
+            norm, edges = np.histogramdd(sample=(np.concatenate(coord1samples), np.concatenate(coord2samples)), bins= (X_edges, Y_edges)  )
+            hist, edges = np.histogramdd(sample=(np.concatenate(coord1samples), np.concatenate(coord2samples)),  bins= (X_edges, Y_edges), weights=np.concatenate(samples))
+            hist /= norm
+            return hist.T
+
+    def convolution(self, std, map_value):
+
+        '''
+        Function to convolve the maps with a gaussian.
+        Parameters
+        ----------
+        std: float
+            std of the gaussian in pixel values
+        Returns
+        -------
+        '''
+
+        kernel = Gaussian2DKernel(x_stddev=std)
+
+        convolved_map = convolve(map_value, kernel)
+
+        return convolved_map
+
+    
