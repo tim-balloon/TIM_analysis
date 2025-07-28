c7e596fa6bf15688dedebc41a346fb4cf056688a
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-07-28 15:18:21 -0500

    update

--- timestream_maker/hitmap_array.py
+++ timestream_maker/hitmap_array.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
 
     aquisition_frequency = P['acquisition_frequency']  #sample per frame defined here as the acquisition rate in Hz. 
     dt = 1/aquisition_frequency/3600*np.pi/3.14 #Make the timestep non rational to avoid some stripes in the hitmap. 
-    spf = np.ceil(aquisition_frequency).astype(int)
+    spf = np.round(aquisition_frequency).astype(int)
 
     tod_file=P['output_path']+f"TOD_{format_duration(P['T_duration'])}.hdf5" #os.getcwd()+'/'+'+P['file'][:-5]+'
     H = h5py.File(tod_file, "a")
