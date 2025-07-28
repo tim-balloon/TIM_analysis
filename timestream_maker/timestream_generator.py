1a9a8f078537fb0c6b1eb8c19a7b44a03b4bd645
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-07-28 10:59:26 -0500

    update spf

--- timestream_maker/timestream_generator.py
+++ timestream_maker/timestream_generator.py
@@ -131,8 +131,9 @@ if __name__ == "__main__":
         hdr = fits.getheader(P['path']+P['file'])
         res = (hdr['CDELT1'] * u.Unit(hdr['CUNIT1'])).to(u.deg).value
     
-    spf = P['acquisition_frequency']  #sample per frame defined here as the acquisition rate in Hz. 
-    dt = 1/spf/3600*np.pi/3.14 #Make the timestep non rational to avoid some stripes in the hitmap. 
+    aquisition_frequency = P['acquisition_frequency']  #sample per frame defined here as the acquisition rate in Hz. 
+    dt = 1/aquisition_frequency/3600*np.pi/3.14 #Make the timestep non rational to avoid some stripes in the hitmap. 
+    spf = np.ceil(aquisition_frequency).astype(int)
 
     tod_file=P['output_path']+f"TOD_{format_duration(P['T_duration'])}.hdf5" #os.getcwd()+'/'+'+P['file'][:-5]+'
     H = h5py.File(tod_file, "a")
