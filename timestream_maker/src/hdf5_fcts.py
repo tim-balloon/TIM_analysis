6709ff71cde263074d0b35325028635ea4653b26
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-10-15 15:35:44 -0500

    update

--- timestream_maker/src/hdf5_fcts.py
+++ timestream_maker/src/hdf5_fcts.py
@@ -1,8 +1,9 @@
 import h5py
 import pandas as pd
 import os
+import numpy as np
 
-def save_scan_path(tod_file, scan_path, spf, acquisition_frequency, keys):
+def save_scan_path(tod_file, scan_path, spf, acquisition_frequency, keys,save="32bit"):
     """
     Save the scan path in the .hdf5 format. 
 
@@ -28,12 +29,16 @@ def save_scan_path(tod_file, scan_path, spf, acquisition_frequency, keys):
         if('data' in grp): del grp['data'] 
         if('spf' in grp): del grp['spf'] 
         if('acquisition frequency' in grp): del grp['acquisition frequency'] 
+
+        if(save=="32bit"): coord = (coord).astype(np.float32)
+        elif(save=="16bit"): coord = (coord).astype(np.float16)
+
         grp.create_dataset('data', data=coord, compression='gzip', compression_opts=9)
         grp.create_dataset('spf', data=spf)
         grp.create_dataset('acquisition frequency', data=acquisition_frequency)
     H.close() 
 
-def save_timestamps(tod_file, T, spf, acquisition_frequency, key):
+def save_timestamps(tod_file, T, spf, acquisition_frequency, key, save="32bit"):
     '''
     Save the time tod in the .hdf5 format. 
 
@@ -57,13 +62,17 @@ def save_timestamps(tod_file, T, spf, acquisition_frequency, key):
     if('data' in grp): del grp['data'] 
     if('spf' in grp): del grp['spf'] 
     if('acquisition frequency' in grp): del grp['acquisition frequency'] 
+
+    if(save=="32bit"): T = (T).astype(np.float32)
+    elif(save=="16bit"): T = (T).astype(np.float16)
+    
     grp.create_dataset('data', data=T, compression='gzip', compression_opts=9)
     grp.create_dataset('spf', data=spf)
     grp.create_dataset('acquisition frequency', data=acquisition_frequency)
 
     H.close()
 
-def save_tod_in_hdf5(tod_file, det_names, samples, pixel_offset, pixel_shift, dect_file, F, spf, acquisition_frequency):
+def save_tod_in_hdf5(tod_file, det_names, samples, pixel_offset, pixel_shift, dect_file, F, spf, acquisition_frequency, save="32bit"):
     """
     Save the tod for one array of TIM detectors in the .hdf5 format. 
 
@@ -96,7 +105,7 @@ def save_tod_in_hdf5(tod_file, det_names, samples, pixel_offset, pixel_shift, de
     H = h5py.File(tod_file, "a")
 
     for detector, (offset, shift, name) in enumerate(zip(pixel_offset, pixel_shift, det_names)):
-        
+            
         namegrp = f'kid_{name}_roach'
         if namegrp not in H: grp = H.create_group(namegrp)
         else:                grp = H[namegrp]
@@ -106,13 +115,27 @@ def save_tod_in_hdf5(tod_file, det_names, samples, pixel_offset, pixel_shift, de
         if('pixel_offset_x' in grp): del grp['pixel_offset_x'] 
         if('frequency' in grp): del grp['frequency'] 
         if('acquisition frequency' in grp): del grp['acquisition frequency'] 
-        grp.create_dataset('data', data=samples[detector,:],
-                            compression='gzip', compression_opts=9)
-        grp.create_dataset('spf', data=spf)
-        grp.create_dataset('pixel_offset_y', data=offset)
-        grp.create_dataset('pixel_offset_x', data=shift)
-        grp.create_dataset('acquisition frequency', data=acquisition_frequency)
-
+        if(save=="32bit"):
+            sample = (samples[detector,:]).astype(np.float32)
+            grp.create_dataset('data', data=sample, compression='gzip', compression_opts=9)
+            grp.create_dataset('spf',   data=np.float32(spf))                  
+            grp.create_dataset('pixel_offset_y', data=np.float32(offset))
+            grp.create_dataset('pixel_offset_x', data=np.float32(shift))
+            grp.create_dataset('acquisition frequency', data=np.float32(acquisition_frequency))
+        elif(save=="16bit"):
+            sample = (samples[detector,:]).astype(np.float16)
+            grp.create_dataset('data', data=sample, compression='gzip', compression_opts=9)
+            grp.create_dataset('spf', data=np.float16(spf))
+            grp.create_dataset('pixel_offset_y', data=np.float16(offset))
+            grp.create_dataset('pixel_offset_x', data=np.float16(shift))
+            grp.create_dataset('acquisition frequency', data=np.float16(acquisition_frequency))
+        else: 
+            sample = samples[detector,:]
+            grp.create_dataset('data', data=sample, compression='gzip', compression_opts=9)
+            grp.create_dataset('spf', data=spf)
+            grp.create_dataset('pixel_offset_y', data=offset)
+            grp.create_dataset('pixel_offset_x', data=shift)
+            grp.create_dataset('acquisition frequency', data=acquisition_frequency)
     H.close()
 
     if( not os.path.isfile(dect_file) ):
