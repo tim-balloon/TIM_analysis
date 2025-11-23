400e8304ac355b24292d5254ce9ab3fc07add679
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2025-11-22 20:22:11 -0600

    udpate focus

--- namap/src/beam.py
+++ namap/src/beam.py
@@ -3,6 +3,7 @@ from scipy.linalg import svd
 from scipy.optimize import least_squares
 from photutils import find_peaks
 from astropy.stats import sigma_clipped_stats
+from IPython import embed
 
 class beam(object):
 
@@ -11,8 +12,14 @@ class beam(object):
         self.data = data
         self.param = param
 
-        self.xgrid = np.arange(len(self.data[0,:]))
-        self.ygrid = np.arange(len(self.data[:,0]))
+
+        shape = self.data.shape
+        if(len(shape) == 2):
+            self.xgrid = np.arange(shape[1])
+            self.ygrid = np.arange(shape[0])
+        else: 
+            self.xgrid = np.arange(shape[2])
+            self.ygrid = np.arange(shape[1])
         self.xy_mesh = np.meshgrid(self.xgrid,self.ygrid)
 
     def multivariate_gaussian_2d(self, params):
@@ -43,44 +50,86 @@ class beam(object):
 
     def peak_finder(self, map_data, mask_pf = False):
 
+        """
+        Find peaks in a 2D map, build Gaussian initial guesses,
+        and update an exclusion mask to avoid double detections.
+        """
+
+        # Get number of pixels along each grid axis
         x_lim = np.size(self.xgrid)
         y_lim = np.size(self.ygrid)
+
+        # Factor to determine the peak-finding box size
         fact = 20.
 
-        bs = np.array([int(np.floor(y_lim/fact)), int(np.floor(x_lim/fact))])
+        # Peak-finding box size (height, width),
+        # roughly map-size/20 in each direction
+        bs = np.array([int(np.floor(y_lim / fact)),
+                    int(np.floor(x_lim / fact))])
 
+        # Compute sigma-clipped statistics of the full dataset
         mean, median, std = sigma_clipped_stats(self.data, sigma=3.0)
-        threshold = median+(5.*std)
+
+        # Detection threshold = median + 5σ
+        threshold = median + (5. * std)
+
+        # If no mask provided, create a fresh empty mask
         if mask_pf is False:
             tbl = find_peaks(map_data, threshold, box_size=bs)
             mask_pf = np.zeros_like(self.xy_mesh[0])
         else:
+            # If mask given, store and use it to avoid picking masked regions
             self.mask = mask_pf.copy()
-            tbl = find_peaks(map_data, threshold, box_size=bs, mask = self.mask)
+            tbl = find_peaks(map_data, threshold, box_size=bs, mask=self.mask)
+
+        # Formatting for printing the peak values
         tbl['peak_value'].info.format = '%.8g'
 
+        # Arrays to collect initial Gaussian guesses
         guess = np.array([])
 
+        # Arrays that store the x,y positions of detected peaks
         x_i = np.array([])
         y_i = np.array([])
 
+        # Loop over detected peaks
         for i in range(len(tbl['peak_value'])):
-            guess_temp = np.array([tbl['peak_value'][i], self.xgrid[tbl['x_peak'][i]], \
-                                  self.ygrid[tbl['y_peak'][i]], 1., 1., 0.])
+
+            # Construct initial guess parameters for a 2D Gaussian:
+            #   amplitude, x0, y0, sigma_x, sigma_y, correlation
+            guess_temp = np.array([
+                tbl['peak_value'][i],
+                self.xgrid[tbl['x_peak'][i]],
+                self.ygrid[tbl['y_peak'][i]],
+                1., 1., 0.
+            ])
+
+            # Append these parameters to the global guess array
             guess = np.append(guess, guess_temp)
+
+            # Extract x,y index positions of the peak
             index_x = self.xgrid[tbl['x_peak'][i]]
             index_y = self.ygrid[tbl['y_peak'][i]]
+
+            # Store peak positions
             x_i = np.append(x_i, index_x)
             y_i = np.append(y_i, index_y)
-            mask_pf[index_y-bs[1]:index_y+bs[1], index_x-bs[0]:index_x+bs[0]] = True
 
+            # Mark a rectangular region around the peak as "used"
+            # to prevent re-identifying peaks in the same area
+            mask_pf[index_y - bs[1] : index_y + bs[1],
+                    index_x - bs[0] : index_x + bs[0]] = True
+
+            # Initialize or append to self.param and self.mask
             if self.param is None:
+                # First peak detected → initialize parameter array
                 self.param = guess_temp
                 self.mask = mask_pf.copy()
-
             else:
+                # Additional peaks → append parameters and update mask
                 self.param = np.append(self.param, guess_temp)
                 self.mask = np.logical_or(self.mask, mask_pf)
+
 
     def fit(self):
         try:
@@ -103,7 +152,10 @@ class beam(object):
             msg = 'Too Many parameters',
             return msg, 0
 
+
     def beam_fit(self, mask_pf= False):
+
+        embed()
 
         if self.param is not None:
             peak_found = np.size(self.param)/6
